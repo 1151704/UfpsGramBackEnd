1a4440929992163fe6881069bde8f099ac68f636
Omar Ramón Montes <omarrm@ufps.edu.co>
2019-04-11 19:56:31 -0500

    Modificar cors Configuracion

--- src/main/java/omarrm/ufps/web/apirest/MvcConfig.java
+++ src/main/java/omarrm/ufps/web/apirest/MvcConfig.java
@@ -1,10 +1,15 @@
 package omarrm.ufps.web.apirest;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import omarrm.ufps.web.apirest.auth.CustomAccessDeniedHandler;
@@ -26,6 +31,20 @@ public class MvcConfig implements WebMvcConfigurer {
 	@Bean
 	public AuthenticationEntryPoint authenticationEntryPoint() {
 		return new CustomAuthenticationEntryPoint();
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedHeaders(Arrays.asList("Content-Type", "authorization"));
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+		configuration.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+
 	}
 
 }
--- src/main/java/omarrm/ufps/web/apirest/SpringSecurityConfig.java
+++ src/main/java/omarrm/ufps/web/apirest/SpringSecurityConfig.java
@@ -1,9 +1,6 @@
 package omarrm.ufps.web.apirest;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -14,9 +11,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import omarrm.ufps.web.apirest.auth.filter.JWTAuthenticationFilter;
 import omarrm.ufps.web.apirest.auth.filter.JWTAuthorizationFilter;
@@ -42,11 +37,14 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private AuthenticationEntryPoint authenticationEntryPoint;
 	
+	@Autowired
+	private CorsConfigurationSource corsConfigurationSource;
+	
 	@Override
 	protected void configure(final HttpSecurity http) throws Exception {
 		http
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-		.cors().configurationSource(corsConfigurationSource()).and()
+		.cors().configurationSource(corsConfigurationSource).and()
 		.csrf().disable()
 		.authorizeRequests()
         .antMatchers(HttpMethod.POST, "/user/").permitAll()
@@ -64,20 +62,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 		build.userDetailsService(userDetailsService)
 		.passwordEncoder(passwordEncoder);
 		 		
-	}
-	
-	@Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedHeaders(Arrays.asList("Content-Type", "authorization"));
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
-        configuration.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-	
+	}	
 	
 
 }
